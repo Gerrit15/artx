2c7b6cc9487fe1052288f6776899c04630a3eac3
Gerrit15 <overlorde159@gmail.com>
2024-10-06 23:10:25 -0400

    did the entire rest of it I guess

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,34 @@
 use std::fmt;
-use std::io::stdin;
+use std::{thread, time};
+use std::io::{stdout, Write};
+use crossterm::{QueueableCommand, cursor};
+use rustyline::{DefaultEditor, Result};
 
-fn main() {
-    let mut b = Board::new();
-    b.board[3][4] = Cell::ALIVE;
-    b.board[3][5] = Cell::ALIVE;
-    b.board[3][6] = Cell::ALIVE;
-    let mut s = String::new();
+fn main() -> Result<()> {
+    let input = Board::get_input();
+    let mut b = Board::new(input.2, input.3);
+
+    for i in input.1 {
+        if i.2 == "g".to_string() {b.make_glider(i.0, i.1)}
+        if i.2 == "p".to_string() {b.make_pulsar(i.0, i.1)}
+        if i.2 == "c".to_string() {b.make_copperhead(i.0, i.1)}
+    }
+
+    let mut stdout = stdout();
     loop {
+        let _ = stdout.flush();
+        let _ = stdout.queue(cursor::MoveTo(0, 0));
         println!("{b}");
         b.board = b.update();
-        stdin().read_line(&mut s).unwrap();
+        //stdin().read_line(&mut s).unwrap();
+        thread::sleep(time::Duration::from_secs_f32(input.0));
+
     }
+
+    #[allow(unreachable_code)]
+    Ok(())
 }
 
-#[derive(Default)]
 struct Board {
     pub width: usize,
     pub height: usize,
@@ -22,12 +36,179 @@ struct Board {
 }
 
 impl Board {
-    pub fn new() -> Board {
+    pub fn new(x: usize, y: usize) -> Board {
         Board {
-            width: 10,
-            height: 10,
-            board: vec![vec![Cell::DEAD; 10];10],
+            width: x,
+            height: y,
+            board: vec![vec![Cell::DEAD; y];x],
         }
+    }
+
+    pub fn get_input() -> (f32, Vec<(usize, usize, String)>, usize, usize) {
+        let mut rl = DefaultEditor::new().unwrap();
+        let mut timestep = 0.0;
+        let mut opts = vec![];
+        let mut xmax: usize = 0;
+        let mut ymax: usize = 0;
+
+        let input = rl.readline(">>Timestep: ");
+        match input {
+            Ok(n) => timestep = n.trim().parse().unwrap_or(0.25),
+            Err(rustyline::error::ReadlineError::Eof) => return (timestep, opts, xmax, ymax),
+            _ => ()
+        }
+
+        let input = rl.readline(">>X maximum: ");
+        match input {
+            Ok(n) => xmax = n.trim().parse().unwrap_or(10),
+            Err(rustyline::error::ReadlineError::Eof) => return (timestep, opts, xmax, ymax),
+            _ => ()
+        }
+
+        let input = rl.readline(">>Y maximum: ");
+        match input {
+            Ok(n) => ymax = n.trim().parse().unwrap_or(10),
+            Err(rustyline::error::ReadlineError::Eof) => return (timestep, opts, xmax, ymax),
+            _ => ()
+        }
+
+        loop {
+            let mut vals = (0, 0, "".to_string());
+            let object_type = rl.readline(">>Type [g, p, c]: ");
+            match object_type {
+                Ok(s) => vals.2 = s,
+                Err(rustyline::error::ReadlineError::Eof) => return (timestep, opts, xmax, ymax),
+                _ => ()
+            }
+            let x_loc = rl.readline(">>X location: ");
+            match x_loc {
+                Ok(n) => vals.0 = n.trim().parse().unwrap_or(0),
+                Err(rustyline::error::ReadlineError::Eof) => return (timestep, opts, xmax, ymax),
+                _ => ()
+            }
+            let y_loc = rl.readline(">>Y location: ");
+            match y_loc {
+                Ok(n) => vals.1 =  n.trim().parse().unwrap_or(0),
+                Err(rustyline::error::ReadlineError::Eof) => return (timestep, opts, xmax, ymax),
+                _ => ()
+            }
+            opts.push(vals)
+        }
+    }
+
+    pub fn make_copperhead(&mut self, x: usize, y: usize) {
+        self.board[0 + x][3 + y] = Cell::ALIVE;
+        self.board[0 + x][4 + y] = Cell::ALIVE;
+        self.board[1 + x][3 + y] = Cell::ALIVE;
+        self.board[1 + x][4 + y] = Cell::ALIVE;
+
+        self.board[3 + x][2 + y] = Cell::ALIVE;
+        self.board[3 + x][3 + y] = Cell::ALIVE;
+        self.board[3 + x][4 + y] = Cell::ALIVE;
+        self.board[3 + x][5 + y] = Cell::ALIVE;
+
+
+        self.board[4 + x][1 + y] = Cell::ALIVE;
+        self.board[4 + x][2 + y] = Cell::ALIVE;
+        self.board[4 + x][5 + y] = Cell::ALIVE;
+        self.board[4 + x][6 + y] = Cell::ALIVE;
+
+
+        self.board[5 + x][0 + y] = Cell::ALIVE;
+        self.board[5 + x][7 + y] = Cell::ALIVE;
+
+
+        self.board[7 + x][0 + y] = Cell::ALIVE;
+        self.board[7 + x][7 + y] = Cell::ALIVE;
+        self.board[8 + x][0 + y] = Cell::ALIVE;
+        self.board[8 + x][7 + y] = Cell::ALIVE;
+
+
+        self.board[8 + x][2 + y] = Cell::ALIVE;
+        self.board[8 + x][5 + y] = Cell::ALIVE;
+
+        self.board[9 + x][3 + y] = Cell::ALIVE;
+        self.board[9 + x][4 + y] = Cell::ALIVE;
+        self.board[10 + x][3 + y] = Cell::ALIVE;
+        self.board[10 + x][4 + y] = Cell::ALIVE;
+
+        self.board[11 + x][1 + y] = Cell::ALIVE;
+        self.board[11 + x][2 + y] = Cell::ALIVE;
+        self.board[11 + x][5 + y] = Cell::ALIVE;
+        self.board[11 + x][6 + y] = Cell::ALIVE;
+    }
+
+    pub fn make_pulsar(&mut self, x: usize, y: usize) {
+        self.board[0 + x][2 + y] = Cell::ALIVE;
+        self.board[0 + x][3 + y] = Cell::ALIVE;
+        self.board[0 + x][4 + y] = Cell::ALIVE;
+
+        self.board[0 + x][8 + y] = Cell::ALIVE;
+        self.board[0 + x][9 + y] = Cell::ALIVE;
+        self.board[0 + x][10 + y] = Cell::ALIVE;
+
+        self.board[2 + x][0 + y] = Cell::ALIVE;
+        self.board[2 + x][5 + y] = Cell::ALIVE;
+        self.board[2 + x][7 + y] = Cell::ALIVE;
+        self.board[2 + x][12 + y] = Cell::ALIVE;
+
+        self.board[3 + x][0 + y] = Cell::ALIVE;
+        self.board[3 + x][5 + y] = Cell::ALIVE;
+        self.board[3 + x][7 + y] = Cell::ALIVE;
+        self.board[3 + x][12 + y] = Cell::ALIVE;
+
+        self.board[4 + x][0 + y] = Cell::ALIVE;
+        self.board[4 + x][5 + y] = Cell::ALIVE;
+        self.board[4 + x][7 + y] = Cell::ALIVE;
+        self.board[4 + x][12 + y] = Cell::ALIVE;
+
+        self.board[5 + x][2 + y] = Cell::ALIVE;
+        self.board[5 + x][3 + y] = Cell::ALIVE;
+        self.board[5 + x][4 + y] = Cell::ALIVE;
+
+        self.board[5 + x][8 + y] = Cell::ALIVE;
+        self.board[5 + x][9 + y] = Cell::ALIVE;
+        self.board[5 + x][10 + y] = Cell::ALIVE;
+
+        self.board[7 + x][2 + y] = Cell::ALIVE;
+        self.board[7 + x][3 + y] = Cell::ALIVE;
+        self.board[7 + x][4 + y] = Cell::ALIVE;
+
+        self.board[7 + x][8 + y] = Cell::ALIVE;
+        self.board[7 + x][9 + y] = Cell::ALIVE;
+        self.board[7 + x][10 + y] = Cell::ALIVE;
+
+        self.board[8 + x][0 + y] = Cell::ALIVE;
+        self.board[8 + x][5 + y] = Cell::ALIVE;
+        self.board[8 + x][7 + y] = Cell::ALIVE;
+        self.board[8 + x][12 + y] = Cell::ALIVE;
+
+        self.board[9 + x][0 + y] = Cell::ALIVE;
+        self.board[9 + x][5 + y] = Cell::ALIVE;
+        self.board[9 + x][7 + y] = Cell::ALIVE;
+        self.board[9 + x][12 + y] = Cell::ALIVE;
+
+        self.board[10 + x][0 + y] = Cell::ALIVE;
+        self.board[10 + x][5 + y] = Cell::ALIVE;
+        self.board[10 + x][7 + y] = Cell::ALIVE;
+        self.board[10 + x][12 + y] = Cell::ALIVE;
+
+        self.board[12 + x][2 + y] = Cell::ALIVE;
+        self.board[12 + x][3 + y] = Cell::ALIVE;
+        self.board[12 + x][4 + y] = Cell::ALIVE;
+
+        self.board[12 + x][8 + y] = Cell::ALIVE;
+        self.board[12 + x][9 + y] = Cell::ALIVE;
+        self.board[12 + x][10 + y] = Cell::ALIVE;
+    }
+
+    pub fn make_glider(&mut self, x: usize, y: usize) {
+        self.board[3 + x][0 + y] = Cell::ALIVE;
+        self.board[3 + x][1 + y] = Cell::ALIVE;
+        self.board[3 + x][2 + y] = Cell::ALIVE;
+        self.board[2 + x][2 + y] = Cell::ALIVE;
+        self.board[1 + x][1 + y] = Cell::ALIVE;
+
     }
 
     pub fn update(&self) -> Vec<Vec<Cell>> {
@@ -76,11 +257,11 @@ impl fmt::Display for Board {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut out = "".to_owned();
         for i in &self.board {
-            let mut s = "|".to_owned();
+            let mut s = "".to_owned();
             for j in i {
                 match j {
-                    Cell::DEAD => s += " [ ] |",
-                    Cell::ALIVE => s += " [X] |",
+                    Cell::DEAD => s += "██",
+                    Cell::ALIVE => s += "\x1b[32m██\x1b[0m",
                 }
             }
             out += &(s + "\n");
@@ -125,7 +306,7 @@ impl Cell {
         }).sum();
 
         if self == &Cell::ALIVE {
-            if k > 2 {Cell::DEAD}
+            if k < 2 {Cell::DEAD}
             else if k == 2 || k == 3 {Cell::ALIVE}
             else {Cell::DEAD}
         } else {
